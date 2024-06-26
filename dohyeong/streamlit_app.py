6c52a59fc4374d07406d5f16ace926560e7a12d0
dopa <dohan2038@gmail.com>
2024-06-27 00:05:45 +0900

    proto

--- dohyeong/streamlit_app.py
+++ dohyeong/streamlit_app.py
@@ -4,20 +4,18 @@ import requests
 
 st.title("데이터 입력 및 처리")
 
-with st.form("input_form"):
-    브랜드명 = st.text_input("브랜드명")
-    업종_선택 = st.selectbox("업종 선택", ["value1", "value2", "value3"])
-    선택 = st.selectbox("선택", ["value1", "value2", "value3"])
-    브랜드_소개 = st.text_area("브랜드 소개")
+with (st.form("input_form")):
+    분류 = st.selectbox("업종 선택", ["화장품", "식품\제과", "전기전자"])
+    브랜드 = st.text_input("브랜드")
+    스타일 = st.selectbox("선택", ["감성", "언어유희", "선언"])
     submitted = st.form_submit_button("작성")
 
 if submitted:
     # 입력된 데이터를 JSON 형태로 생성
     input_data = {
-        "브랜드명": 브랜드명,
-        "업종_선택": 업종_선택,
-        "선택": 선택,
-        "브랜드_소개": 브랜드_소개
+        "분류": 분류,
+        "브랜드": 브랜드,
+        "스타일": 스타일
     }
 
     # FastAPI 서버로 데이터 전송
@@ -26,5 +24,7 @@ if submitted:
     if response.status_code == 200:
         response_data = response.json()
         st.success("응답 받음: {}".format(response_data))
+
+        st.write(response_data)
     else:
         st.error("오류 발생: {}".format(response.status_code))
