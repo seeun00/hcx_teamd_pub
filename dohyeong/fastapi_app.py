2871df56c53661648f3778297da3d9a882aec169
seeun00 <endqo123@gmail.com>
2024-07-08 22:23:44 +0900

    demo-done

--- dohyeong/fastapi_app.py
+++ dohyeong/fastapi_app.py
@@ -1,6 +1,122 @@
+# # fastapi_app.py
+# import json
+# import os
+
+# from fastapi import FastAPI, HTTPException
+# from pydantic import BaseModel
+# import requests
+
+# app = FastAPI()
+
+# class CompletionExecutor:
+#     def __init__(self):
+#         self._host = 'https://clovastudio.stream.ntruss.com'
+#         self._api_key = os.getenv('API_KEY')
+#         self._api_key_primary_val = os.getenv('API_KEY_PRIMARY_VAL')
+#         self._request_id = os.getenv('REQUEST_ID')
+
+#     def execute(self, completion_request):
+#         headers = {
+#             'X-NCP-CLOVASTUDIO-API-KEY': self._api_key,
+#             'X-NCP-APIGW-API-KEY': self._api_key_primary_val,
+#             'X-NCP-CLOVASTUDIO-REQUEST-ID': self._request_id,
+#             'Content-Type': 'application/json; charset=utf-8',
+#             'Accept': 'text/event-stream'
+#         }
+
+#         with requests.post('https://clovastudio.stream.ntruss.com/testapp/v1/tasks/a3r5nl4j/chat-completions',
+#                            headers=headers, json=completion_request, stream=True) as r:
+#             event_result_found = False
+#             for line in r.iter_lines():
+#                 if line:
+#                     decoded_line = line.decode('utf-8')
+#                     if event_result_found:
+#                         json_part = decoded_line.split('data:', 1)[1]
+#                         json_data = json.loads(json_part)
+#                         return json_data["message"]["content"]
+#                     if decoded_line.startswith("event:result"):
+#                         event_result_found = True
+
+# class InputData(BaseModel):
+#     분류: str
+#     브랜드: str
+#     스타일: str
+
+# def format_fix(data):
+#     formatted_data = ""
+#     for key, value in data.items():
+#         formatted_data += f"{key}: {value}\n"
+#     return formatted_data
+
+
+# @app.post("/execute")
+# def execute(data: InputData):
+#     # 예시로 단순히 데이터를 반환합니다.
+#     # 실제 응답 처리 로직을 이곳에 작성합니다.
+#     response = {
+#         "message": "데이터가 성공적으로 처리되었습니다.",
+#         "received_data": data.dict()
+#     }
+#     print("received_data : " + format_fix(response['received_data']))
+
+#     completion_executor = CompletionExecutor()
+
+#     preset_text = [{"role": "system",
+#                     "content": "당신은 수년간의 경험을 가진 전문 광고 카피라이터입니다. 3가지 항목을 사용자 입력으로 받아 창의적이고 매력적인 광고 문구를 작성해주세요.\n\n\n광고 카피를 생성할 때 다음 내용을 반영하세요.\n\r\n1. 브랜드의 톤앤매너를 반영할 것\r\n2. 핵심 메시지를 효과적으로 전달할 것\r\n3. 간결하면서도 임팩트 있는 표현을 사용할 것\r\n4. 광고 문구에 브랜드명을 포함할 것\n\n\n3가지 사용자 입력은 다음과 같습니다.\n1. 분류: 브랜드의 업종 분류\n2. 브랜드: 브랜드 이름\n3. 스타일: 광고 문구의 스타일\n스타일은 다음을 의미합니다.\n- 선점 : 다른 브랜드 대비 선점\n- 기능 : 제품의 기능을 강조\n- 감성 : 제품의 감성적인 터치를 강조\r\n- 사운드 : 사운드 효과 강조\r\n- 언어유희 : 동음이의어 등 언어 유희 이용\n- 영어: 영어를 이용\n\n\n###\n분류: 화장품\n브랜드: 이니스프리\n스타일: 감성\n광고카피: 나의 제주 이야기가 스며든 피부, 이니스프리\n###\n분류: 식품/제과\n브랜드: 코카콜라\n스타일: 언어유희\n광고카피: 코카콜라와 함께 무한탄산 무한텐션\n###\n분류: 정보통신\n브랜드: G마켓\n스타일: 선점\n광고카피: 지상 최대의 선물마켓, G마켓\n###\n분류: 전기전자\n브랜드: 애플 아이폰\n스타일: 영어\n광고카피: Your New Superpower\n###\n\n"},
+#                    {"role": "user", "content": format_fix(response['received_data'])}]
+
+#     request_data = {
+#         'messages': preset_text,
+#         'topP': 0.8,
+#         'topK': 0,
+#         'maxTokens': 256,
+#         'temperature': 1.0,
+#         'repeatPenalty': 5.0,
+#         'stopBefore': ["###"],
+#         'includeAiFilters': True,
+#         'seed': 0
+#     }
+
+#     # print(preset_text)
+#     result_str = completion_executor.execute(request_data)
+#     print(result_str)
+#     print("-------------------------------------")
+#     print(parse_event_data(result_str))
+#     print("-------------------------------------")
+    
+#     result = {
+#         "message": "데이터가 성공적으로 처리되었습니다.",
+#         "data": result_str
+#     }
+#     return result
+
+
+# def parse_event_data(event_data: str) -> str:
+#     # Split the data into lines
+#     lines = event_data.split('\n')
+#     print(lines)
+#     # Iterate through lines to find the line that starts with 'event:result'
+#     for i, line in enumerate(lines):
+
+#         if line.startswith("event:result"):
+#             # The next line contains the data we need
+#             result_line = lines[i + 1]
+#             # Extract the JSON part of the result_line
+#             json_part = result_line.split('data:', 1)[1]
+#             # Load the JSON data
+#             json_data = json.loads(json_part)
+#             # Return the content value
+#             return json_data["message"]["content"]
+
+#     return ""
+
+
+
+
+###################################
+# fastapi_app.py
 # fastapi_app.py
 import json
-import os
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
@@ -8,12 +124,13 @@ import requests
 
 app = FastAPI()
 
+
 class CompletionExecutor:
-    def __init__(self):
-        self._host = 'https://clovastudio.stream.ntruss.com'
-        self._api_key = os.getenv('API_KEY')
-        self._api_key_primary_val = os.getenv('API_KEY_PRIMARY_VAL')
-        self._request_id = os.getenv('REQUEST_ID')
+    def __init__(self, host, api_key, api_key_primary_val, request_id):
+        self._host = host 
+        self._api_key = api_key
+        self._api_key_primary_val = api_key_primary_val
+        self._request_id = request_id
 
     def execute(self, completion_request):
         headers = {
@@ -59,7 +176,12 @@ def execute(data: InputData):
     }
     print("received_data : " + format_fix(response['received_data']))
 
-    completion_executor = CompletionExecutor()
+    completion_executor = CompletionExecutor(
+        host='https://clovastudio.stream.ntruss.com',
+        api_key='NTA0MjU2MWZlZTcxNDJiY5hZx6dN1Tpv3ZTB8YSA6lz7UXUZlVA9R6yULgExuyf6',
+        api_key_primary_val='YLRxXUhJANdAcQpY9KW9JygbX5Nf54mHHAQw1wdy',
+        request_id='8893cd2a-d18f-45c6-8d8e-82df4e920e3c'
+    )
 
     preset_text = [{"role": "system",
                     "content": "당신은 수년간의 경험을 가진 전문 광고 카피라이터입니다. 3가지 항목을 사용자 입력으로 받아 창의적이고 매력적인 광고 문구를 작성해주세요.\n\n\n광고 카피를 생성할 때 다음 내용을 반영하세요.\n\r\n1. 브랜드의 톤앤매너를 반영할 것\r\n2. 핵심 메시지를 효과적으로 전달할 것\r\n3. 간결하면서도 임팩트 있는 표현을 사용할 것\r\n4. 광고 문구에 브랜드명을 포함할 것\n\n\n3가지 사용자 입력은 다음과 같습니다.\n1. 분류: 브랜드의 업종 분류\n2. 브랜드: 브랜드 이름\n3. 스타일: 광고 문구의 스타일\n스타일은 다음을 의미합니다.\n- 선점 : 다른 브랜드 대비 선점\n- 기능 : 제품의 기능을 강조\n- 감성 : 제품의 감성적인 터치를 강조\r\n- 사운드 : 사운드 효과 강조\r\n- 언어유희 : 동음이의어 등 언어 유희 이용\n- 영어: 영어를 이용\n\n\n###\n분류: 화장품\n브랜드: 이니스프리\n스타일: 감성\n광고카피: 나의 제주 이야기가 스며든 피부, 이니스프리\n###\n분류: 식품/제과\n브랜드: 코카콜라\n스타일: 언어유희\n광고카피: 코카콜라와 함께 무한탄산 무한텐션\n###\n분류: 정보통신\n브랜드: G마켓\n스타일: 선점\n광고카피: 지상 최대의 선물마켓, G마켓\n###\n분류: 전기전자\n브랜드: 애플 아이폰\n스타일: 영어\n광고카피: Your New Superpower\n###\n\n"},
--- dohyeong/streamlit_app.py
+++ dohyeong/streamlit_app.py
@@ -56,114 +56,195 @@ import streamlit as st
 import requests
 from PIL import Image, ImageDraw, ImageOps
 
-fastapi_url = "https://seeun.pythonanywhere.com"
-st.markdown(f"""
-    <style>
-
-    .stButton button {{
-        background-color: #e91e63;
-        color: white;
-        border: none;
-        padding: 0.5em 1em;
-        border-radius: 5px;
-    }}
-    </style>
-""", unsafe_allow_html=True)
-
-def get_image_as_base64(file_path: str, output_size: tuple) -> str:
-    with Image.open(file_path) as img:
-        img = img.resize(output_size)
-
-        buffered = io.BytesIO()
-        img.save(buffered, format="WEBP")
-        return base64.b64encode(buffered.getvalue()).decode('utf-8')
-image_path = "./dumbd_image.webp"
-output_size = (400,400)
-image_base64 = get_image_as_base64(image_path, output_size)
-
-
 
 st.markdown('<div class="container">', unsafe_allow_html=True)
-st.markdown('<h1>외로운 대행사의 카피라이터, 김카피</h1>', unsafe_allow_html=True)
+# st.markdown("""<h1>외로운 대행사의 카피라이터, 김카피</h1></div>""", unsafe_allow_html=True)
 # st.markdown(f'<img src="data:image/webp;base64,{image_base64}" alt="profile-pic">', unsafe_allow_html=True)
 
-st.markdown(
-    f"""
+st.markdown("""
     <style>
-    .centered {{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        # height: 100vh;
-    }}
+    .centered {
+        text-align: center;
+        margin: auto;
+    }
+    h1 {
+        font-size: 35px;
+    }
     </style>
     <div class="centered">
-        <img src="data:image/webp;base64,{image_base64}" alt="centered image">
+        <h1>외로운 대행사의 카피라이터, 김카피</h1>
     </div>
-    """,
-    unsafe_allow_html=True
-)
-st.markdown('<br>', unsafe_allow_html=True)
+""", unsafe_allow_html=True)
+
+
+left_container,middle_space, right_container = st.columns([1, 0.2, 1])
+
+with left_container:
+    st.markdown(f"""
+        <style>
+
+        .stButton button {{
+            background-color: #e91e63;
+            color: white;
+            border: none;
+            padding: 0.5em 1em;
+            border-radius: 5px;
+        }}
+        </style>
+    """, unsafe_allow_html=True)
+
+    def get_image_as_base64(file_path: str, output_size: tuple) -> str:
+        with Image.open(file_path) as img:
+            img = img.resize(output_size)
+
+            buffered = io.BytesIO()
+            img.save(buffered, format="WEBP")
+            return base64.b64encode(buffered.getvalue()).decode('utf-8')
+    image_path = "./dumbd_image.webp"
+    output_size = (400,400)
+    image_base64 = get_image_as_base64(image_path, output_size)
+
+
+
+    # st.markdown('<div class="container">', unsafe_allow_html=True)
+    # # st.markdown("""<h1>외로운 대행사의 카피라이터, 김카피</h1></div>""", unsafe_allow_html=True)
+    # # st.markdown(f'<img src="data:image/webp;base64,{image_base64}" alt="profile-pic">', unsafe_allow_html=True)
+
+    # st.markdown("""
+    #     <style>
+    #     .centered {
+    #         text-align: center;
+    #         margin: auto;
+    #     }
+    #     h1 {
+    #         font-size: 35px;
+    #     }
+    #     </style>
+    #     <div class="centered">
+    #         <h1>외로운 대행사의 카피라이터, 김카피</h1>
+    #     </div>
+    # """, unsafe_allow_html=True)
+
+    st.markdown('<br>', unsafe_allow_html=True)
+
+    st.markdown(
+        f"""
+        <style>
+        .centered {{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            # height: 100vh;
+        }}
+        </style>
+        <div class="centered">
+            <img src="data:image/webp;base64,{image_base64}" alt="centered image">
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+
+    st.markdown('<br>', unsafe_allow_html=True)
+
+    st.markdown("""
+    <style>
+        .center-text {
+            text-align: center;
+            font-size: 16px;
+            font-family: Arial, sans-serif;
+        }
+        </style>
+    <p class="center-text">또 왔네? 광고 카피가 그냥 자판기 버튼 누르면 나오는 줄 아는구나.. 응 실은 맞아....
+    <br><br>*최근 광고 카피를 학습하여 입력한 정보에 알맞은 카피를 제공합니다.</p>
+
+    """, unsafe_allow_html=True)
+
+with middle_space:
+    st.write()
+
+with right_container:
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+
+    with (st.form("input_form")):
+        분류 = st.selectbox("업종 선택", ["관공서/단체", "기업PR", "생활/가정용품", "서비스/유통/레저", "식품/제과", "자동차/정유", "전기전자", "정보통신", "출판/교육/문화", "패션/스포츠", "화장품"])
+        브랜드 = st.text_input("브랜드 입력")
+        스타일 = st.selectbox("스타일 선택", ["감성", "기능", "사운드", "선언", "선점", "언어유희","영어"])
+        submitted = st.form_submit_button("작성")
+
+
+    # st.markdown("""
+    # <style>
+    #     .center-description {
+    #         text-align: center;
+    #         font-size: 16px;
+    #         font-family: Arial, sans-serif;
+    #         color: grey;
+    #     }
+    #     </style>
+    # <p class="center-description"><br><br><br>본 서비스는 네이버클라우드플랫폼의 하이퍼클로바X로 만들었습니다.</p>
+
+    # """, unsafe_allow_html=True)
+
+
+    if submitted:
+        # 입력된 데이터를 JSON 형태로 생성
+        input_data = {
+            "분류": 분류,
+            "브랜드": 브랜드,
+            "스타일": 스타일
+        }
+
+        # FastAPI 서버로 데이터 전송
+        response = requests.post("http://localhost:8000/execute", json=input_data)
+
+        if response.status_code == 200:
+            response_data = response.json()
+            # st.success("응답 받음: {}".format(response_data))
+
+            result_box = f"""
+                <style>
+                .result-box {{
+                    border: 2px solid #4CAF50;
+                    padding: 10px;
+                    border-radius: 5px;
+                    # background-color: #f9f9f9;
+                    margin-top: 20px;
+                    font-family: Arial, sans-serif;
+                }}
+                .result-title {{
+                    font-size: 20px;
+                    font-weight: bold;
+                    color: #4CAF50;
+                }}
+                .result-content {{
+                    margin-top: 10px;
+                    font-size: 20px;
+                }}
+                </style>
+                <div class="result-box">
+                    <div class="result-title">카피라이팅 결과</div>
+                    <div class="result-content">{response_data['data']}</div>
+                </div>
+                """
+
+            # 결과 폼 표시
+            st.markdown(result_box, unsafe_allow_html=True)
+        else:
+            st.error("오류 발생: {}".format(response.status_code))
+
 st.markdown("""
 <style>
-    .center-text {
+    .center-description {
         text-align: center;
         font-size: 16px;
         font-family: Arial, sans-serif;
+        color: grey;
     }
     </style>
-<p class="center-text">또 왔네? 광고 카피가 그냥 자판기 버튼 누르면 나오는 줄 아는구나.. 응 실은 맞아....
-<br><br>*최근 광고 카피를 학습하여 알맞은 카피를 제공합니다. 최대한 많은 정보를 제공해주세요.</p>
+<p class="center-description"><br><br><br>본 서비스는 네이버클라우드플랫폼의 하이퍼클로바X로 만들었습니다.</p>
 
 """, unsafe_allow_html=True)
-with (st.form("input_form")):
-    분류 = st.selectbox("업종 선택", ["화장품", "식품\제과", "전기전자"])
-    브랜드 = st.text_input("브랜드 입력")
-    스타일 = st.selectbox("스타일 선택", ["감성", "언어유희", "선언"])
-    submitted = st.form_submit_button("작성")
-
-if submitted:
-    # 입력된 데이터를 JSON 형태로 생성
-    input_data = {
-        "분류": 분류,
-        "브랜드": 브랜드,
-        "스타일": 스타일
-    }
-
-    # FastAPI 서버로 데이터 전송
-    response = requests.post(f"{fastapi_url}/execute", json=input_data)
-
-    if response.status_code == 200:
-        response_data = response.json()
-        # st.success("응답 받음: {}".format(response_data))
-
-        result_box = f"""
-            <style>
-            .result-box {{
-                border: 2px solid #4CAF50;
-                padding: 10px;
-                border-radius: 5px;
-                # background-color: #f9f9f9;
-                margin-top: 20px;
-                font-family: Arial, sans-serif;
-            }}
-            .result-title {{
-                font-size: 20px;
-                font-weight: bold;
-                color: #4CAF50;
-            }}
-            .result-content {{
-                margin-top: 10px;
-                font-size: 20px;
-            }}
-            </style>
-            <div class="result-box">
-                <div class="result-title">카피라이팅 결과</div>
-                <div class="result-content">{response_data['data']}</div>
-            </div>
-            """
-
-        # 결과 폼 표시
-        st.markdown(result_box, unsafe_allow_html=True)
-    else:
-        st.error("오류 발생: {}".format(response.status_code))
